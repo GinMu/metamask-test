6881fea2cd9d466deb6c627f5c8b9cd181b52ae9
GinMu <gin.musheng@gmail.com>
2023-10-20 17:05:53 +0800

    metamask test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>Account: {{ account }}</div>
+    <div>Chain Id: {{ chainId }}</div>
+    <button @click="connect">Connect</button>
+    <button @click="switchChain">Switch chain</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+const ethereum = window.ethereum;
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      account: "",
+      chainId: "0x1"
+    }
+  },
+  async mounted() {
+    // this.chainId = ethereum.chainId;
+    await this.connect();
+  },
+  methods: {
+    async connect() {
+      try {
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        console.log('accounts: ', accounts)
+        this.account = accounts[0]
+      } catch (error) {
+        console.log('error: ', error)
+      }
+    },
+    async switchChain() {
+      try {
+        const connected = ethereum.isConnected();
+        console.log("connected: ", connected);
+        const chainId = this.chainId === "0x1" ? "0x38" : "0x1";
+        await ethereum.request({
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId }],
+        })
+        const blockNumber = await ethereum.request({
+          method: "eth_blockNumber",
+          params: []
+        });
+        console.log("block number: ", blockNumber);
+        this.chainId = chainId;
+      } catch (error) {
+        console.log("switch error: ", error);
+      }
+    }
   }
 }
 </script>
